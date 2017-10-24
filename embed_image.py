0dde24cf32ec4d2198b660e92ff222dce00fb45b
unknown <pmorris2012@fau.edu>
2017-10-24 17:37:16 -0400

    working face vector

--- embed_image.py
+++ embed_image.py
@@ -14,26 +14,33 @@ from PIL import Image
 
 class ImageEmbedding():
     def __init__(self, args):
-        with tf.Graph().as_default():
-            with tf.Session() as sess:
-                # Load the model
-                print('Loading feature extraction model')
-                facenet.load_model(args.model)
-                
-                # Get input and output tensors
-                self.images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-                self.embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-                self.phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-                self.embedding_size = embeddings.get_shape()[1]
+        tf.Graph().as_default()
+        self.sess = tf.Session()
+
+        # Load the model
+        print('Loading feature extraction model')
+        facenet.load_model(args["model"], self.sess)
+        
+        # Get input and output tensors
+        self.images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        self.embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        self.phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+        self.embedding_size = self.embeddings.get_shape()[1]
                 
     def embed(self, img):
         # Run forward pass to calculate embeddings
         print('Calculating features for images')
         feed_dict = { self.images_placeholder:img, self.phase_train_placeholder:False }
-        emb = sess.run(embeddings, feed_dict=feed_dict)
-            
+        emb = self.sess.run(self.embeddings, feed_dict=feed_dict)
+        return emb        
+
 imgemb = ImageEmbedding({
     "model": "20170512-110547/"
 })
-img = np.asarray(Image.open("shia.jpg"))
+img = Image.open("shia.jpg")
+img = img.resize((160, 160))
+img = np.asarray(img)
+img = np.reshape(img, [1, 160, 160, 3])
 print(img.shape)
+
+print(imgemb.embed(img))
