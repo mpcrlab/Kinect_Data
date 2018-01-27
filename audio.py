d326d02df3257a638afd620ece8455fa31d2474e
Paul Morris <pmorris2012@fau.edu>
2018-01-27 01:16:59 -0500

    working audio

--- audio.py
+++ audio.py
@@ -1,5 +1,30 @@
 import pyaudio
+import numpy as np
+import wave
 
 p = pyaudio.PyAudio()
+wf = wave.open("alert.wav", 'rb')
 
-stream = p.open(
+def callback(in_data, frame_count, time_info, status):
+    data = wf.readframes(frame_count)
+    return (data, pyaudio.paContinue)
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()), 
+                channels=wf.getnchannels(), rate=wf.getframerate(), 
+                output=True, stream_callback=callback)
+stream.stop_stream()
+  
+def start_tone():
+  global stream
+  wf.setpos(0)
+  stream.start_stream()
+  
+def stop_tone():
+  global stream
+  stream.stop_stream()
+
+def exit():
+  gloabl stream, p
+  stream.close()
+  p.terminate()
+  
